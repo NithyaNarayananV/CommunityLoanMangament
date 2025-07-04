fd931f5e9600f1fd337458262ae96863a9db1255
Nithya Narayanan <NithyaNarayananCVR@gmail.com>
2025-07-04 18:41:26 +0530

    Mojar Update - Correction

--- force-app/main/default/lwc/cNS_LWC_MasterDataDisplay/cNS_LWC_MasterDataDisplay.js
+++ force-app/main/default/lwc/cNS_LWC_MasterDataDisplay/cNS_LWC_MasterDataDisplay.js
@@ -1,30 +1,47 @@
-import { LightningElement , wire} from 'lwc';
+import { LightningElement } from 'lwc';
 import getTotalLoanBalance from '@salesforce/apex/CNS_LWC_HelperClass.getTotalActiveBalanceLoan';
 import getTotalShareCount from '@salesforce/apex/CNS_LWC_HelperClass.getTotalSharesCount';
+import getSangamMemberCount from '@salesforce/apex/CNS_LWC_HelperClass.getSangamMembersCount';
+import getSangamTotalExpenses from '@salesforce/apex/CNS_LWC_HelperClass.getSangamTotalExpenses';
 
 export default class CNS_LWC_MasterDataDisplay extends LightningElement {
     activeLoanBalance = 0;
-    @wire(getTotalLoanBalance)
-    wiredBalance({ error, data }) {
-        if (data) {
-            this.activeLoanBalance = data;
-        } else if (error) {
-            console.error('Error fetching balance:', error);
-        }
-    }
     totalShares = 0;
     totalSharesValue = 0;
-    @wire(getTotalShareCount)
-    wiredBalance({ error, data }) {
-        if (data) {
-            this.totalShares = data;
-            this.totalSharesValue= this.totalShares * 5; // each share is worth 5
-        } else if (error) {
-            console.error('Error fetching balance:', error);
-        }
+    sangamMember = 0;
+    totalExpenses =0;
+
+    connectedCallback() {
+        this.fetchAllMetrics();
     }
 
+    handleRefresh() {
+        this.fetchAllMetrics(); // Trigger refresh from the top icon
+    }
 
+    fetchAllMetrics() {
+        getTotalLoanBalance()
+            .then(result => {
+                this.activeLoanBalance = result;
+            })
+            .catch(error => console.error('Loan balance error:', error));
 
-    //
+        getTotalShareCount()
+            .then(result => {
+                this.totalShares = result;
+                this.totalSharesValue = this.totalShares * 5;
+            })
+            .catch(error => console.error('Shares count error:', error));
+
+        getSangamMemberCount()
+            .then(result => {
+                this.sangamMember = result;
+            })
+            .catch(error => console.error('Member count error:', error));
+        getSangamTotalExpenses()
+            .then(result => {
+                this.totalExpenses = result;
+            })
+            .catch(error => console.error('Total Expense error:', error));
+    }
 }
